3fdf5882b9b207d75cd581021868a78b616f864e
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2020-09-08 16:48:21 -0400

    Mock fs tests

--- packages/core/src/base/tests/fs.test.ts
+++ packages/core/src/base/tests/fs.test.ts
@@ -1,12 +1,29 @@
 import {join} from 'path';
-import {remove} from 'fs-extra';
 import {FileSystem} from '../fs';
 
-const root = join(__dirname, 'fixtures', 'fs');
-const rm = async (file: string) => remove(join(root, file));
+jest.mock('fs-extra');
+jest.mock('glob');
+
+const {
+  writeFile: mockWriteFile,
+  readFile: mockReadFile,
+  mkdirp: mockMkdirp,
+  copy: mockCopy,
+} = jest.requireMock('fs-extra');
+const {sync: mockGlob} = jest.requireMock('glob');
+
+const root = join(__dirname, 'mock', 'root');
 
 describe('FileSystem', () => {
   const fileSystem = new FileSystem(root);
+
+  beforeEach(() => {
+    mockWriteFile.mockReset();
+    mockReadFile.mockReset();
+    mockMkdirp.mockReset();
+    mockCopy.mockReset();
+    mockGlob.mockReset();
+  });
 
   describe('root', () => {
     it('is provided path', () => {
@@ -16,87 +33,110 @@ describe('FileSystem', () => {
 
   describe('read', () => {
     it('reads file', async () => {
+      mockReadFile.mockReturnValue(`{ "foo": "bar" }`);
+
       const contents = await fileSystem.read('file.json');
 
-      expect(contents).toBe(`{ "foo": "bar" }\n`);
+      expect(contents).toBe(`{ "foo": "bar" }`);
+      expect(mockReadFile).toHaveBeenCalledWith(
+        join(root, 'file.json'),
+        'utf8',
+      );
     });
   });
 
   describe('write', () => {
-    const fileName = 'something.txt';
-    const nestedFileName = 'some/thing.txt';
-
-    afterEach(async () => {
-      await rm(fileName);
-      await rm('some');
-    });
-
     it('writes file', async () => {
+      const fileName = 'something.txt';
       const contents = 'hi, world';
       await fileSystem.write(fileName, contents);
-      const fileContents = await fileSystem.read(fileName);
 
-      expect(fileContents).toBe(contents);
+      expect(mockMkdirp).toHaveBeenCalledWith(root);
+      expect(mockWriteFile).toHaveBeenCalledWith(
+        join(root, fileName),
+        contents,
+      );
     });
 
     it('writes nested file', async () => {
+      const fileName = 'some/thing.txt';
       const contents = 'hi, world';
-      await fileSystem.write(nestedFileName, contents);
-      const fileContents = await fileSystem.read(nestedFileName);
+      await fileSystem.write(fileName, contents);
 
-      expect(fileContents).toBe(contents);
+      expect(mockMkdirp).toHaveBeenCalledWith(join(root, 'some'));
+      expect(mockWriteFile).toHaveBeenCalledWith(
+        join(root, fileName),
+        contents,
+      );
     });
   });
 
   describe('copy', () => {
-    const toFile = 'copy.json';
-    const toDir = 'copied-folder';
-
-    afterEach(async () => {
-      await rm(toFile);
-      await rm(toDir);
-    });
-
     it('copies file', async () => {
       const fromFile = 'file.json';
+      const toFile = 'copy.json';
       await fileSystem.copy(fromFile, toFile);
-      const fileContents = await fileSystem.read(toFile);
 
-      expect(fileContents).toBe(`{ "foo": "bar" }\n`);
+      expect(mockCopy).toHaveBeenCalledWith(
+        join(root, fromFile),
+        join(root, toFile),
+        undefined,
+      );
     });
 
     it('copies directory', async () => {
       const fromDir = 'folder';
+      const toDir = 'copied-folder';
       await fileSystem.copy(fromDir, toDir);
-      const dirGlob = await fileSystem.glob(`${toDir}/*`);
-      const [file] = dirGlob;
 
-      expect(file).toMatch('copied-folder/file.json');
+      expect(mockCopy).toHaveBeenCalledWith(
+        join(root, fromDir),
+        join(root, toDir),
+        undefined,
+      );
     });
   });
 
   describe('hasFile', () => {
     it('detects file', async () => {
+      mockGlob.mockReturnValue([join(root, 'file.json')]);
       expect(await fileSystem.hasFile('file.json')).toBe(true);
     });
 
     it('ignores directory', async () => {
+      mockGlob.mockReturnValue([]);
       expect(await fileSystem.hasFile('folder')).toBe(false);
+      expect(mockGlob).toHaveBeenCalledWith('folder', {
+        cwd: fileSystem.root,
+        absolute: true,
+        nodir: true,
+      });
     });
   });
 
   describe('hasDirectory', () => {
     it('detects directory', async () => {
+      mockGlob.mockReturnValue([join(root, 'folder/')]);
       expect(await fileSystem.hasDirectory('folder')).toBe(true);
     });
 
     it('ignores file', async () => {
+      mockGlob.mockReturnValue([]);
       expect(await fileSystem.hasDirectory('file.json')).toBe(false);
+      expect(mockGlob).toHaveBeenCalledWith('file.json/', {
+        cwd: fileSystem.root,
+        absolute: true,
+      });
     });
   });
 
   describe('glob', () => {
     it('globs', async () => {
+      mockGlob.mockReturnValue([
+        join(root, 'file.json'),
+        join(root, 'folder/'),
+        join(root, 'folder/file.json'),
+      ]);
       const glob = await fileSystem.glob('**/*');
       const [file, folder, nestedFile] = glob;
 
