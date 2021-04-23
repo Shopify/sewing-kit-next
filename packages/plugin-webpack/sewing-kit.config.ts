45c52cd95dee8483d8d1249ee70643d7596c57eb
Ben Scott <ben.scott@shopify.com>
2021-04-22 18:22:20 -0700

    plugin-webpack: create entrypoint for noop
    This file referenced via require.resolve, but never imported directly,
    so explicitly add an entrypoint for it (needed for rollup)

--- packages/plugin-webpack/sewing-kit.config.ts
+++ packages/plugin-webpack/sewing-kit.config.ts
@@ -3,5 +3,7 @@ import {createSewingKitPackagePlugin} from '../../config/sewing-kit';
 
 export default createPackage((pkg) => {
   pkg.runtime(Runtime.Node);
+  pkg.entry({root: './src/index'});
+  pkg.entry({name: 'noop', root: './src/noop'});
   pkg.use(createSewingKitPackagePlugin());
 });
