36f5cd42de5750f430daa0772ead327ddc605e66
Ben Scott <ben.scott@shopify.com>
2021-07-06 23:07:25 -0700

    Remove caching

--- packages/plugin-typescript/src/plugin-typescript.ts
+++ packages/plugin-typescript/src/plugin-typescript.ts
@@ -1,13 +1,10 @@
-import {join, resolve} from 'path';
-
-import {copy} from 'fs-extra';
 import {
-  Workspace,
   WaterfallHook,
   DiagnosticError,
   createProjectPlugin,
   createWorkspacePlugin,
   WorkspacePluginContext,
+  addHooks,
 } from '@sewing-kit/core';
 
 import type {} from '@sewing-kit/plugin-javascript';
@@ -63,11 +60,12 @@ export function workspaceTypeScript() {
       tasks: {build, typeCheck},
     } = context;
 
-    build.hook(({hooks, options}) => {
-      hooks.configureHooks.hook((hooks: any) => ({
-        ...hooks,
-        typescriptHeap: new WaterfallHook(),
-      }));
+    build.hook(({hooks}) => {
+      hooks.configureHooks.hook(
+        addHooks<TypeScriptTypeCheckingHooks>(() => ({
+          typescriptHeap: new WaterfallHook(),
+        })),
+      );
 
       // We don’t build TypeScript definitions for workspaces that only include
       // web apps or services
@@ -79,176 +77,42 @@ export function workspaceTypeScript() {
       }
 
       hooks.pre.hook((steps, {configuration}) => {
-        const newSteps = [...steps];
-
-        if (options.cache) {
-          newSteps.push(createLoadTypeScriptCacheStep(context));
-        }
-
-        newSteps.push(createRunTypeScriptStep(context, configuration));
-
-        return newSteps;
+        return [...steps, createRunTypeScriptStep(context, configuration)];
       });
-
-      if (options.cache) {
-        hooks.post.hook((steps) => [...steps, createCacheSaveStep(context)]);
-      }
     });
 
-    typeCheck.hook(({hooks, options}) => {
-      hooks.configureHooks.hook((hooks) => ({
-        ...hooks,
-        typescriptHeap: new WaterfallHook(),
-      }));
+    typeCheck.hook(({hooks}) => {
+      hooks.configureHooks.hook(
+        addHooks<TypeScriptTypeCheckingHooks>(() => ({
+          typescriptHeap: new WaterfallHook(),
+        })),
+      );
 
-      hooks.pre.hook((steps) => {
-        const newSteps = [...steps];
-
-        if (options.cache) {
-          newSteps.push(createLoadTypeScriptCacheStep(context));
-        }
-
-        return newSteps;
+      hooks.steps.hook((steps, {configuration}) => {
+        return [...steps, createRunTypeScriptStep(context, configuration)];
       });
-
-      hooks.steps.hook((steps, {configuration}) => [
-        ...steps,
-        createRunTypeScriptStep(context, configuration),
-      ]);
-
-      if (options.cache) {
-        hooks.post.hook((steps) => [...steps, createCacheSaveStep(context)]);
-      }
     });
   });
 }
 
-const OUTPUT_DIRECTORY_NAME = 'output';
-const BUILD_DIRECTORY_CACHE_FILENAME = 'info';
-const TSBUILDINFO_FILE = 'tsconfig.tsbuildinfo';
-
-function createCacheSaveStep({workspace, api}: WorkspacePluginContext) {
-  return api.createStep(
-    {
-      id: 'TypeScript.SaveCache',
-      label: 'save typescript cache',
-    },
-    async () => {
-      try {
-        const {references = []} = JSON.parse(
-          await workspace.fs.read('tsconfig.json'),
-        ) as {references?: {path: string}[]};
-
-        await Promise.all(
-          references.map(async ({path: reference}) => {
-            const outDirectory = await getTscOutputDirectory(
-              reference,
-              workspace,
-            );
-            const projectCacheDirectory = join(
-              api.cachePath('typescript'),
-              reference.replace(/^\.*\/?/, '').replace(/\//g, '_'),
-            );
-            const cacheOutputDirectory = join(
-              projectCacheDirectory,
-              OUTPUT_DIRECTORY_NAME,
-            );
-
-            await workspace.fs.write(
-              join(projectCacheDirectory, TSBUILDINFO_FILE),
-              await workspace.fs.read(
-                resolve(outDirectory, `../${TSBUILDINFO_FILE}`),
-              ),
-            );
-
-            await workspace.fs.write(
-              join(projectCacheDirectory, BUILD_DIRECTORY_CACHE_FILENAME),
-              outDirectory,
-            );
-
-            await copy(
-              workspace.fs.resolvePath(reference, outDirectory),
-              cacheOutputDirectory,
-              {preserveTimestamps: true},
-            );
-          }),
-        );
-      } catch {
-        // noop
-      }
-    },
-  );
-}
-
-async function getTscOutputDirectory(project: string, workspace: Workspace) {
-  const tsconfig = JSON.parse(
-    await workspace.fs.read(workspace.fs.resolvePath(project, 'tsconfig.json')),
-  ) as {compilerOptions?: {outDir?: string}};
-
-  return workspace.fs.resolvePath(
-    project,
-    tsconfig.compilerOptions?.outDir ?? 'build/ts',
-  );
-}
-
-function createLoadTypeScriptCacheStep({
-  workspace,
-  api,
-}: WorkspacePluginContext) {
-  return api.createStep(
-    {
-      id: 'TypeScript.RestoreCache',
-      label: 'restore typescript cache',
-    },
-    async () => {
-      try {
-        const projectCacheDirectories = await workspace.fs.glob(
-          join(api.cachePath('typescript'), '*/'),
-        );
-
-        await Promise.all(
-          projectCacheDirectories.map(async (projectCacheDirectory) => {
-            const outDirectory = await workspace.fs.read(
-              join(projectCacheDirectory, BUILD_DIRECTORY_CACHE_FILENAME),
-            );
-
-            await copy(
-              join(projectCacheDirectory, TSBUILDINFO_FILE),
-              resolve(outDirectory, `../${TSBUILDINFO_FILE}`),
-              {preserveTimestamps: true},
-            );
-
-            await copy(
-              join(projectCacheDirectory, OUTPUT_DIRECTORY_NAME),
-              outDirectory,
-              {preserveTimestamps: true},
-            );
-          }),
-        );
-      } catch {
-        // noop
-      }
-    },
-  );
-}
-
 export function createRunTypeScriptStep(
-  {api}: Pick<WorkspacePluginContext, 'api'>,
-  configure: Partial<TypeScriptTypeCheckingHooks>,
+  context: WorkspacePluginContext,
+  configuration: Partial<TypeScriptTypeCheckingHooks>,
 ) {
-  return api.createStep(
+  return context.api.createStep(
     {
       id: 'TypeScript.TypeCheck',
       label: 'run typescript',
     },
     async (step) => {
-      const heap = await configure.typescriptHeap!.run(0);
+      const heap = await configuration.typescriptHeap!.run(0);
       const heapArguments = heap ? [`--max-old-space-size=${heap}`] : [];
 
+      // emit = true;
       try {
         await step.exec(
-          'node',
-          [...heapArguments, 'node_modules/.bin/tsc', '--build', '--pretty'],
+          'node_modules/.bin/tsc',
+          [...heapArguments, '--build'],
           {all: true, env: {FORCE_COLOR: '1'}},
         );
       } catch (error) {
