480913d8826bdc0067db2195eb9165f00a48c8ca
Ben Scott <227292+BPScott@users.noreply.github.com>
2021-08-04 10:27:25 -0700

    Upddat to lerna 4 (#224)

--- tests/consistent-package-json.test.ts
+++ tests/consistent-package-json.test.ts
@@ -60,7 +60,7 @@ packages.forEach(
         it('specifies publishable files, including index entrypoints', () => {
           // Don't use arrayContaining here as it does not guarantee order
           // We want to make sure the first set of items are always in a fixed
-          // order. Most importantly, the `!*.tsbuildinfo` exclusion must be
+          // order. Most importantly, the `!build/*.tsbuildinfo` exclusion must be
           // after `build/*`.
           /* eslint-disable jest/no-if */
           if (packageJSON?.bin) {
