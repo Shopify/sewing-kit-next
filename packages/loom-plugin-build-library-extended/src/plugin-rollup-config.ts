41d8591a269ae82f4771947d287567e93aa48217
Ben Scott <ben.scott@shopify.com>
2021-09-17 17:46:36 -0700

    Rewrite rollupConfig to avoid dependency on plugin-build-library
    Use the rollup hooks directly, so we only need the optional peerdep on
    plugin-rollup for the types rather than needing a harder dependency on
    plugin-build-library
    
    Add a half-baked sass test to prove that build-library-extended works

--- packages/loom-plugin-build-library-extended/src/plugin-rollup-config.ts
+++ packages/loom-plugin-build-library-extended/src/plugin-rollup-config.ts
@@ -1,9 +1,11 @@
-import {rollupPlugins} from '@shopify/loom-plugin-build-library';
+import {createProjectBuildPlugin} from '@shopify/loom';
 import postcssShopify from '@shopify/postcss-plugin';
 import pluginGraphql from '@rollup/plugin-graphql';
 import json from '@rollup/plugin-json';
 import image from '@rollup/plugin-image';
 import svgr from '@svgr/rollup';
+
+import type {} from '@shopify/loom-plugin-rollup';
 
 import {styles} from './rollup/rollup-plugin-styles';
 
@@ -12,25 +14,38 @@ interface RollupConfigOptions {
 }
 
 export function rollupConfig({graphql = false}: RollupConfigOptions) {
-  return rollupPlugins((target) => {
-    const stylesConfig = target.options.rollupEsnext
-      ? {
-          mode: 'esnext',
-          plugins: [postcssShopify],
-        }
-      : {
-          mode: 'standalone',
-          output: 'styles.css',
-          plugins: [postcssShopify],
-        };
+  return createProjectBuildPlugin(
+    'Loom.BuildLibraryExtended.RollupConfig',
+    ({hooks}) => {
+      hooks.target.hook(({hooks, target}) => {
+        hooks.configure.hook((configuration) => {
+          configuration.rollupPlugins?.hook((rollupPlugins) => {
+            const stylesConfig = target.options.rollupEsnext
+              ? {
+                  mode: 'esnext',
+                  plugins: [postcssShopify],
+                }
+              : {
+                  mode: 'standalone',
+                  output: 'styles.css',
+                  plugins: [postcssShopify],
+                };
 
-    return [
-      json(),
-      graphql && pluginGraphql(),
-      // TODO fix the svgr plugin casting as any when Plugin signature is compatible with rollup Plugin signature
-      svgr({include: '**/icons/*.svg', exclude: '', babel: true}) as any,
-      image({exclude: '**/icons/*.svg'}),
-      styles(stylesConfig),
-    ];
-  });
+            return rollupPlugins.concat([
+              json(),
+              graphql && pluginGraphql(),
+              // TODO fix the svgr plugin casting as any when Plugin signature is compatible with rollup Plugin signature
+              svgr({
+                include: '**/icons/*.svg',
+                exclude: '',
+                babel: true,
+              }) as any,
+              image({exclude: '**/icons/*.svg'}),
+              styles(stylesConfig),
+            ]);
+          });
+        });
+      });
+    },
+  );
 }
